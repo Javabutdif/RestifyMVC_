b91145d21b5677c484f31e009a7b1dba9ab2c48b
Anton James Genabio <jamesgenabio31@gmail.com>
2024-05-04 21:27:19 +0800

    update scaffold

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcLandlord.Data;
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<MvcLandlordContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MvcLandlordContext") ?? throw new InvalidOperationException("Connection string 'MvcLandlordContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
